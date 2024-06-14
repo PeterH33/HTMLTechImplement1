1380b82e0c62997cebd71fdd626f4c3ec3716ea8
Peter Hartnett <peter.hartnett@gmail.com>
2024-06-14 13:29:20 -0400

    adding handler.php

--- MyFirstHandler.php
+++ MyFirstHandler.php
@@ -4,7 +4,29 @@
 </head>
     <?php
     print_r($_POST);
+    //input
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $address = $_POST['address'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $zipCode = $_POST['zipCode'];
+    $colors = $_POST['colors'];
+    $favNumber = $_POST['favNumber'];
+    $day = $_POST['day'];
 
+
+
+    //processing
+
+
+
+    //output
+    print"Hello $firstName $lastName<br><br>";
+    print"of: $address<br>";
+    print"    $city, $state $zipCode<br><br>";
+    print"You would love $favNumber $colors candies on a $day.<br>";
+    print"I know this because I am a robot and you are a soft flesh creature";
 
 
     ?>
