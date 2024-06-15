5fd617bd5c7ec9b06499cd60713a1635d0635f91
Peter Hartnett <peter.hartnett@gmail.com>
2024-06-15 13:14:36 -0400

    playing with php and css

--- MyFirstHandler.php
+++ MyFirstHandler.php
@@ -12,46 +12,48 @@ $colors = $_POST['colors'];
 $favNumber = $_POST['favNumber'];
 $day = $_POST['day'];
 
-
-
 //processing
 $tense = (int)$favNumber > 1 ? "candies" : "candy";
-
 ?>
 
-<!DOCTYPE html>
-<header>
-    <?= $firstName, $lastName; ?>
-    <br>
-    Plain words
-</header>
 
-<body>
-    body
-</body>
+
+
+<!DOCTYPE html>
+<html>
+
+    <header>
+        <?= $firstName, $lastName; ?>
+        <br>
+        Plain words
+    </header>
+    
+    <body>
+        body
+    </body>
     <!-- //output
     echo"This is just me typing out words, what does it do?";
     print"Another line just like echo, what is the difference?";
     echo"<pre>"; //the <pre> tag causes white space to be preserved, so I get the spacing for the address correct.
-    print"Hello $firstName $lastName<br><br>";
-    print"of: $address<br>";
-    print"    $city, $state $zipCode<br><br>";
-    print"You would love $favNumber $colors $tense on a $day.<br>";
-    print"I know this because I am a robot and you are a soft flesh creature.<br><br>";
-
-    //output testing wrapping html in an echo line
-    echo "
-    
-    
-    ";
-
-
-    //random test of print as a variable assignment
-    $printMe = print("Printing a variable? Or perhaps just true.<br>");
-    echo $printMe; //This line will print out the above statement
-    $printMe //This version returns 1, which I am assuming is basically True, neat! -->
-<footer>
-    Footer
-</footer>
-    
+        print"Hello $firstName $lastName<br><br>";
+        print"of: $address<br>";
+        print"    $city, $state $zipCode<br><br>";
+        print"You would love $favNumber $colors $tense on a $day.<br>";
+        print"I know this because I am a robot and you are a soft flesh creature.<br><br>";
+        
+        //output testing wrapping html in an echo line
+        echo "
+        
+        
+        ";
+        
+        
+        //random test of print as a variable assignment
+        $printMe = print("Printing a variable? Or perhaps just true.<br>");
+        echo $printMe; //This line will print out the above statement
+        $printMe //This version returns 1, which I am assuming is basically True, neat! -->
+        <footer>
+            Footer
+        </footer>
+        
 </html>
