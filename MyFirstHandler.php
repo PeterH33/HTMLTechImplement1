fd9b338c094d4b7ed889da6e9f1befab1092e8fa
Peter Hartnett <peter.hartnett@gmail.com>
2024-06-15 20:59:23 -0400

    playing with php and css

--- MyFirstHandler.php
+++ MyFirstHandler.php
@@ -50,6 +50,7 @@
         <div class="container">
             <div class="span-4">
                 <h4><?=$address;?></h4>
+                <p>Test words</p>
             </div>
             <div class="span-2">
                 <p><?=$city;?><p>
