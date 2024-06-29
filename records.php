797807b7461b047aad36be89b2f3c1ef645a25ba
Peter Hartnett <peter.hartnett@gmail.com>
2024-06-29 11:12:26 -0400

    small corrections

--- records.php
+++ records.php
@@ -8,36 +8,34 @@ function sanitizeString($var)
 }
 ?>
 
+<!DOCTYPE html>
 <html>
-<link rel="stylesheet" href="CSS/myStyle.css">
 <head>
+    <link rel="stylesheet" href="CSS/myStyle.css">
     <style>
         th, td {
             border-bottom: 1px solid gray;
         }
-        table, th, td{
+        table, th, td {
             border-collapse: collapse;
             padding: 10px;
         }
     </style>
     <title>Records</title>
 </head>
-
 <body>
 
-
     <?php
-    //This section for the table
-    //Open connection
+    // This section for the table
+    // Open connection
     $DBConnect = mysqli_connect("127.0.0.1", "sqlimp", "pword", "mysqlimp");
-    if($DBConnect == false)
-    {
-        //DB error here
-
+    if ($DBConnect == false) {
+        // DB error here
+        echo "Failed to connect to the database.";
     } else {
-        //Successful dbConnect logic here
+        // Successful dbConnect logic here
 
-        //Default Show everything
+        // Default Show everything
         $SQLString = "SELECT 
         p.personID, 
         p.firstName, 
@@ -56,52 +54,42 @@ function sanitizeString($var)
         JOIN states s ON p.stateKey = s.stateKey 
         JOIN zipCodes z ON p.zipCodeKey = z.zipCodeKey 
         JOIN colors co ON p.colorKey = co.colorKey 
-        JOIN days d ON p.dayKey = d.dayKey
-        ";
+        JOIN days d ON p.dayKey = d.dayKey";
 
-        //Run the query
+        // Run the query
         $queryResult = mysqli_query($DBConnect, $SQLString);
 
-        if (mysqli_num_rows($queryResult) > 0){
-            //success print something
-            print"List of Records";
-            print"<table width = '100%'>";
-            print"<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Favorite Color</th><th>Favorite Day</th><th>Favorite Number</th><th>Time Stamp</th></tr>";
-            while ($row = mysqli_fetch_assoc($queryResult)){
-                //Table lines
-                print"<tr> <td>{$row['personID']}</td> <td>{$row['firstName']}</td> <td>{$row['lastName']}</td> <td>{$row['address']}</td> <td>{$row['cityName']}</td> <td>{$row['stateName']}</td> <td>{$row['zipCode']}</td> <td>{$row['favoriteColor']}</td> <td>{$row['favoriteDay']}</td> <td>{$row['favNumber']}</td> <td>{$row['ts']}</td>  </tr>";
+        if (mysqli_num_rows($queryResult) > 0) {
+            // Success print something
+            echo "List of Records";
+            echo "<table width='100%'>";
+            echo "<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Favorite Color</th><th>Favorite Day</th><th>Favorite Number</th><th>Time Stamp</th></tr>";
+            while ($row = mysqli_fetch_assoc($queryResult)) {
+                // Table lines
+                echo "<tr><td>{$row['personID']}</td><td>{$row['firstName']}</td><td>{$row['lastName']}</td><td>{$row['address']}</td><td>{$row['cityName']}</td><td>{$row['stateName']}</td><td>{$row['zipCode']}</td><td>{$row['favoriteColor']}</td><td>{$row['favoriteDay']}</td><td>{$row['favNumber']}</td><td>{$row['ts']}</td></tr>";
             }
+            echo "</table>";
         } else {
-            //failure state
-            print"Error no results";
+            // Failure state
+            echo "Error: no results found.";
         }
 
-        //Remove result from memory
+        // Remove result from memory
         mysqli_free_result($queryResult);
     }
 
-    //close connection
+    // Close connection
     mysqli_close($DBConnect);
     ?>
 
-    
-
-<footer>
-    <hr>
-
-    <div>
+    <footer>
+        <hr>
         <div>
             <img src="FSUlogo.png" alt="">
-
         </div>
         <br>
         HTML and Server/Technology Implementation
-    </div>
-</footer>
-
+    </footer>
 
 </body>
-
-
-
 </html>
