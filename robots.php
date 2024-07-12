d56a0b0ea1366bf14e54578fa33aca1b59a6d571
Peter Hartnett <peter.hartnett@gmail.com>
2024-07-12 15:59:11 -0400

    modifying robots.php

--- robots.php
+++ robots.php
@@ -8,17 +8,33 @@ function sanitizeString($var)
     return $var;
 }
 
+function getIPAddress() {  
+    //whether ip is from the share internet  
+     if(!emptyempty($_SERVER['HTTP_CLIENT_IP'])) {  
+                $ip = $_SERVER['HTTP_CLIENT_IP'];  
+        }  
+    //whether ip is from the proxy  
+    elseif (!emptyempty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+     }  
+    //whether ip is from the remote address  
+    else{  
+             $ip = $_SERVER['REMOTE_ADDR'];  
+     }  
+     return $ip;  
+}  
+
 require 'creepydb.php';
-    $ipAddress = '192.168.1.1';
+    $ipAddress = getIPAddress();
     $lat = 37.7749;
     $lon = -122.4194;
     $city = 'San Francisco';
     $region = 'California';
     $isp = 'ISP Company';
-    $stmt = $mysqli->prepare("INSERT INTO visitors (ipAddress, lat, lon, city, region, isp)
-    VALUES (?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param('sddsss', $ipAddress, $lat, $lon, $city, $region, $isp);
-    $stmt->execute();
+    // $stmt = $mysqli->prepare("INSERT INTO visitors (ipAddress, lat, lon, city, region, isp)
+    // VALUES (?, ?, ?, ?, ?, ?)");
+    // $stmt->bind_param('sddsss', $ipAddress, $lat, $lon, $city, $region, $isp);
+    // $stmt->execute();
     $stmt->close();
 $mysqli->close();
 
@@ -44,7 +60,7 @@ $mysqli->close();
     </header>
 
     <main>
-        <p>Thank you for participating in the human elimination project human number ID of IPADDREES</p>
+        <p>Thank you for participating in the human elimination project human number ID of <?= $ipAddress?></p>
         <p>A T-32000 squad has been dispatched to CITY REGION LAT LON to eliminate all organics.</p>
         <p>And a special thanks to ISP for continuing to support this nobel cause.</p>
 
