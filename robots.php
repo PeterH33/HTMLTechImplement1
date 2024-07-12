97a67bc96a3db48dc075dbf3dccd517c2a25721b
Peter Hartnett <peter.hartnett@gmail.com>
2024-07-12 16:58:01 -0400

    syntax decision do not show id

--- robots.php
+++ robots.php
@@ -70,7 +70,7 @@ $mysqli->close();
     </header>
 
     <main>
-        <p>Thank you for participating in the human elimination project human number ID of <?= $ipAddress?></p>
+        <p>Thank you for participating in the human elimination project human of <?= $ipAddress?></p>
         <p>A T-32000 squad has been dispatched to <?= $city . ", " . $region . " " . $lat . " " . $lon?> to eliminate all organics.</p>
         <p>And a special thanks to <?= $isp?> for continuing to support this noble cause.</p>
 
